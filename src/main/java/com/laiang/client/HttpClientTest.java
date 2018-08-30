ede2910645821a65822f290442a86e7019c6f4f5
laiang <laiang@meili-inc.com>
2018-08-30 16:54:37 +0800

    "java爬虫"

---
+++ src/main/java/com/laiang/client/HttpClientTest.java
@@ -0,0 +1,180 @@
+package com.laiang.client;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+
+public class HttpClientTest {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+//        String result = doGet("http://local.crm.meili-inc.com/");
+//        System.out.println(result);
+
+        Map<String, Object> map = new HashMap();
+        map.put("fuzzy", null);
+        map.put("groupName", null);
+        map.put("level", null);
+        map.put("ownerWorkId", null);
+        map.put("pageNum", 1);
+        map.put("pageSize", 200);
+        map.put("platformId", 38);
+        String result = doPost("http://auth.meili-inc.com/auth/permission/query", map);
+
+        JSONObject obj = JSON.parseObject(result);
+        JSONArray array = obj.getJSONObject("data").getJSONArray("list");
+
+        for(int i = 0 ; i < array.size() ; i++){
+            JSONObject item = array.getJSONObject(i);
+                Map<String, Object> mapto = new HashMap();
+                mapto.put("description", item.getString("description"));
+                mapto.put("id", item.getLong("id"));
+                mapto.put("name", item.getString("name"));
+                mapto.put("ownerWorkId", "00222");
+                mapto.put("platformId", "38");
+                mapto.put("value", item.getString("value"));
+                doPost("http://auth.meili-inc.com/auth/permission/update", mapto);
+        }
+        System.out.println(result);
+    }
+
+    public static String doGet(String url) {
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        String result = "";
+        try {
+            // 通过址默认配置创建一个httpClient实例
+            httpClient = HttpClients.createDefault();
+            // 创建httpGet远程连接实例
+            HttpGet httpGet = new HttpGet(url);
+            // 设置请求头信息，鉴权
+//            httpGet.setHeader("Authorization", "Bearer da3efcbf-0845-4fe3-8aba-ee040be542c0");
+            // 设置配置请求参数
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000)// 连接主机服务超时时间
+                    .setConnectionRequestTimeout(35000)// 请求超时时间
+                    .setSocketTimeout(60000)// 数据读取超时时间
+                    .build();
+            // 为httpGet实例设置配置
+            httpGet.setConfig(requestConfig);
+            // 执行get请求得到返回对象
+            response = httpClient.execute(httpGet);
+            // 通过返回对象获取返回数据
+            HttpEntity entity = response.getEntity();
+            // 通过EntityUtils中的toString方法将结果转换为字符串
+            result = EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != response) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != httpClient) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+
+    }
+
+    public static String doPost(String url, Map<String, Object> paramMap) {
+        CloseableHttpClient httpClient;
+        CloseableHttpResponse httpResponse = null;
+        String result = "";
+        // 创建httpClient实例
+        httpClient = HttpClients.createDefault();
+        // 创建httpPost远程连接实例
+        HttpPost httpPost = new HttpPost(url);
+        // 配置请求参数实例
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000)// 设置连接主机服务超时时间
+                .setConnectionRequestTimeout(35000)// 设置连接请求超时时间
+                .setSocketTimeout(60000)// 设置读取数据连接超时时间
+                .build();
+        // 为httpPost实例设置配置
+        httpPost.setConfig(requestConfig);
+        // 设置请求头
+        httpPost.addHeader("Content-Encoding", "gzip");
+        httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
+        httpPost.addHeader("Cookie", "MTGK=TGK-3133-tl5DLmmHDgD2ejHDLderMs56cytoJC6HDow; _mf=ihCB-VPUT2GRfxLRC-teNw; JSESSIONID=047557CB59A23192D37CA4FF42F1B65F");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
+        httpPost.addHeader("X-Requested-With", "XMLHttpRequest");
+
+
+
+        // 封装post请求参数
+        if (null != paramMap && paramMap.size() > 0) {
+//            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+//            // 通过map集成entrySet方法获取entity
+//            Set<Map.Entry<String, Object>> entrySet = paramMap.entrySet();
+//            // 循环遍历，获取迭代器
+//            Iterator<Map.Entry<String, Object>> iterator = entrySet.iterator();
+//            while (iterator.hasNext()) {
+//                Map.Entry<String, Object> mapEntry = iterator.next();
+//                nvps.add(new BasicNameValuePair(mapEntry.getKey(), mapEntry.getValue().toString()));
+//            }
+
+            //[{ name : 'fuzzy'  , value : null} , { name : 'fuzzy'  , value : null} , { name : 'fuzzy'  , value : null}]
+            //{ fuzzy: null , abc: '1321' }
+            // 为httpPost设置封装好的请求参数,同时设置编码，编码很重要
+            httpPost.setEntity(new StringEntity(JSON.toJSONString(paramMap), "utf-8"));
+        }
+        try {
+            // httpClient对象执行post请求,并返回响应参数对象
+            httpResponse = httpClient.execute(httpPost);
+            // 从响应对象中获取响应内容
+            HttpEntity entity = httpResponse.getEntity();
+            result = EntityUtils.toString(entity);
+            if(httpResponse.getStatusLine().getStatusCode() != 200){
+                System.err.println(result);
+                throw new RuntimeException("aaaa");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != httpResponse) {
+                try {
+                    httpResponse.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != httpClient) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+}
